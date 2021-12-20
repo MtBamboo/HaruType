45867ab1444deff367e687454f0e729001c3fefc
MtBamboo <iwannafreefree@yahoo.co.jp>
2021-12-21 07:46:36 +0900

    ゲーム部分のひな形作成
    タイトル画面、タイピング画面の挙動を作成した。

--- js/HaruType.js
+++ js/HaruType.js
@@ -1,10 +1,6 @@
 ﻿
-/* 変数 */
-var targetIndex = 0;
-var targetArray = ["a","b","c","d","e","f","g","h","i","j"];
-const targetNum = 10;
-
 /* 関数 */
+// 初期化
 $(document).ready(function(){
   const ua = navigator.userAgent;
   if (ua.indexOf('iPhone') > -1 || (ua.indexOf('Android') > -1 && ua.indexOf('Mobile') > -1)) {
@@ -17,8 +13,10 @@ $(document).ready(function(){
     $("#state").text("タブレットは未対応です＞＜");
   } else {
     // PC
-    prepareToGame();
-
+    prepareToGame(0);
+    updateGameScene(0);
+/*
+  // URLにシード値を持たせて同じタイピング内容で遊べるように検討しようとした名残
     var urlPrm = new Object;
     var urlSearch = location.search.substring(1).split('&');
 
@@ -31,43 +29,115 @@ $(document).ready(function(){
     {
       $("#seedBox").append("<font color='green'>seed:"+urlPrm.seed+"</font>");
     }
-
-    var single = TargetWords[0];
-    var str = "target:"+single.getWord+":"+single.getLen+":"+single.getLatin[0];
-    $("#seedBox").append("<font color='blue'>target:"+str+"</font>");
+*/
   }
 });
 
-$(window).keydown(function(event){
-  var inp = event.key;
-  if(inp == targetArray[targetIndex])
-  {
-    updateColor(targetIndex,"gray");
-    $("#collect").append(targetArray[targetIndex]);
-    targetIndex++;
-    if(targetIndex == targetNum)
-    {
-      $("#gameArea").append("<div align='center'>おめでとうございます！</div>");
-    }
-    else {
-      updateColor(targetIndex,"blue");
-    }
-  }
-  else {
-    updateColor(targetIndex,"red");
-  }
-});
-
-function prepareToGame()
+// ゲーム開始準備
+var wholeArray; // タイピング文字列
+function prepareToGame(kind)
 {
-  for(var i=0; i< 10; i++)
+  wholeArray = "";
+  var targetArray;
+  switch(kind)
   {
-    $("#target").append("<font color='silver'>"+targetArray[i]+"</font>");
+    case 0:
+      targetArray = TargetEWords;
+      break;
+    default:
+      break;
   }
-  updateColor(targetIndex,"blue");      
+
+  for(var i = (targetArray.length - 1 ); 0 < i ; i--)
+  {
+    var r = Math.floor(Math.random() * (i + 1));
+
+    var tmp = targetArray[i];
+    targetArray[i] = targetArray[r];
+    targetArray[r] = tmp;
+  }
+
+  wholeArray += targetArray[0];
+  for(var i=1; i < 5; i++)
+  {
+    wholeArray+=(" " + targetArray[i]);
+  }
+
+  for(var i=0; i < wholeArray.length; i++)
+  {
+    $("#target").append("<font color='silver'>"+wholeArray.charAt(i)+"</font>");
+  }
+  updateColor(targetIndex,"blue",true);
 }
 
-function updateColor(index,color)
+// シーン切り替え
+var Scenes = 
 {
-  $("font").eq(index).css("color", color);
+  title : 0,
+  menu : 1,
+  game : 2
+};
+
+var gameScene;
+function updateGameScene(scene)
+{
+  gameScene = scene;
+  $("#titleBox").css('display', scene == Scenes.title ? 'block' : 'none');
+  $("#menuBox").css('display', scene == Scenes.menu ? 'block' : 'none');
+  $("#gameBox").css('display', scene == Scenes.game ? 'block' : 'none');
+}
+
+// 入力判定
+var targetIndex = 0;
+$(window).keydown(function(event){
+  var inp = event.key;
+
+  if(gameScene == Scenes.title)
+  {
+    // メニュー画面作ろうととしたけど英単語モードだけになったのでカット
+    //updateGameScene(Scenes.menu);
+    updateGameScene(Scenes.game);
+  }
+  else if(gameScene == Scenes.menu)
+  {
+    updateGameScene(2);
+  }
+  else if(gameScene == Scenes.game)
+  {
+    //if(inp == event.shiftKey)
+    if(inp == "Shift")
+    {
+      // 大文字対応、Shiftキー押下時は何もしない
+    }
+    else if(inp == wholeArray.charAt(targetIndex))
+    {
+      updateColor(targetIndex,"gray");
+      targetIndex++;
+      if(targetIndex == wholeArray.length)
+      {
+        // 全ターゲット入力
+        $("#gameBox").append("<div align='center'>おめでとうございます！</div>");
+      }
+      else {
+        updateColor(targetIndex,"blue", true);
+      }
+    }
+    else {
+      updateColor(targetIndex,"red", true);
+    }
+  }
+});
+
+// フォント更新
+function updateColor(index,color,ub)
+{
+  var letter = $("font").eq(index);
+  letter.css("color", color);
+
+  if(ub)
+  {
+    letter.css("textDecoration","underline");
+  }else{
+    letter.css("textDecoration","none");
+  }
 }
